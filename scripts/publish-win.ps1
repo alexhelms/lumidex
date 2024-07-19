5546e87ab98281bea7ea3a33892aa3aa3d6846e5
Alex Helms <alexander.helms@gmail.com>
2024-07-18 22:53:05 -0700

    Use GitVersion instead of nbgv

--- scripts/publish-win.ps1
+++ scripts/publish-win.ps1
@@ -1,3 +1,4 @@
+dotnet tool restore
 dotnet publish Lumidex.Desktop\Lumidex.Desktop.csproj -r win-x64 --self-contained -c Release -o publish-win
-$VERSION = (nbgv get-version)[3].Split(":")[1].Trim()
+$VERSION=(dotnet tool run dotnet-gitversion /showvariable SemVer)
 & 'C:\Program Files (x86)\Inno Setup 6\ISCC.exe' lumidex-installer-win.iss /DMyAppProductVersion=${VERSION}
