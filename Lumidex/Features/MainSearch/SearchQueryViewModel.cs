e86816e0328c1387b1e93e7ed8262701db4c8cf3
Alex Helms <alexander.helms@gmail.com>
2024-07-21 15:01:21 -0700

    Ensure library edits are propagated and user selection is retained.

--- Lumidex/Features/MainSearch/SearchQueryViewModel.cs
+++ Lumidex/Features/MainSearch/SearchQueryViewModel.cs
@@ -8,6 +8,7 @@ namespace Lumidex.Features.MainSearch;
 public partial class SearchQueryViewModel : ViewModelBase,
     IRecipient<LibraryCreated>,
     IRecipient<LibraryDeleted>,
+    IRecipient<LibraryEdited>,
     IRecipient<TagCreated>,
     IRecipient<TagDeleted>
 {
@@ -37,6 +38,23 @@ public partial class SearchQueryViewModel : ViewModelBase,
     public void Receive(LibraryDeleted message)
     {
         Libraries.Remove(message.Library);
+    }
+
+    public void Receive(LibraryEdited message)
+    {
+        if (Libraries.FirstOrDefault(library => library == message.Library) is { } existingLibrary)
+        {
+            // Retain a reference to the selected library, if one exists. Removing it from the list deselects!
+            LibraryViewModel? selectedLibrary = Library;
+
+            var index = Libraries.IndexOf(existingLibrary);
+            Libraries.RemoveAt(index);
+            Libraries.Insert(index, message.Library);
+
+            // Retain user selection
+            if (selectedLibrary is not null && selectedLibrary == message.Library)
+                Library = message.Library;
+        }
     }
 
     public void Receive(TagCreated message)
