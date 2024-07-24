0604e0c944f1d1b6882b0ef1a9b63ac5fc617bc0
Alex Helms <alexander.helms@gmail.com>
2024-07-23 23:05:41 -0700

    Disable the "Open In" context menu items if more than one search results are selected.

---
+++ Lumidex/Converters/FuncConverters.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Data.Converters;
+
+namespace Lumidex.Converters;
+
+public static class FuncConverters
+{
+    public static readonly IValueConverter EqualToOne =
+        new FuncValueConverter<int, bool>(value => value == 1);
+}
