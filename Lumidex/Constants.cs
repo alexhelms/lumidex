fb0e6b4cdaa26cc12802819fc0818532186354b7
Alex Helms <alexander.helms@gmail.com>
2024-11-07 19:25:20 -0700

    Implement Open In Finder and Open In PixInsight for MacOS (#78)

---
+++ Lumidex/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lumidex;
+
+public static class Constants
+{
+    public static string OpenInExplorer
+    {
+        get
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return "Open in Finder";
+            }
+
+            return "Open in Explorer";
+        }
+    }
+}
