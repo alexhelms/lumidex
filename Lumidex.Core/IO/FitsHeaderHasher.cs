0716db717625ad465dccb4b4aeb847d8e14c8b2a
Alex Helms <alexander.helms@gmail.com>
2024-11-12 22:49:33 -0700

    Ignore common hash exception on protected files. (#86)

--- Lumidex.Core/IO/FitsHeaderHasher.cs
+++ Lumidex.Core/IO/FitsHeaderHasher.cs
@@ -5,7 +5,7 @@ namespace Lumidex.Core.IO;
 public class FitsHeaderHasher : HeaderHasher
 {
     // "END" followed by spaces to pad the column to 80 characters.
-    private static byte[] EndRecord = [
+    private static readonly byte[] EndRecord = [
         0x45, 0x4e, 0x44, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
         0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
         0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
@@ -28,31 +28,39 @@ public class FitsHeaderHasher : HeaderHasher
         const int HeaderBlockSize = 2880;
         int count = 0;
 
-        // The buffer is large enough to read most files full headers
-        // and small enough to not go on the large object heap.
-        byte[] fileBuffer = new byte[6 * HeaderBlockSize];
-
-        using var sha1 = SHA1.Create();
-        await using var fs = new FileStream(filename,FileMode.Open, FileAccess.Read);
-        while ((count = await fs.ReadAsync(fileBuffer, token)) != 0)
+        try
         {
-            for (int i = 0; i < count / RecordSize; i++)
+            using var sha1 = SHA1.Create();
+
+            // The buffer is large enough to read most files full headers
+            // and small enough to not go on the large object heap.
+            byte[] fileBuffer = new byte[6 * HeaderBlockSize];
+            
+            await using var fs = new FileStream(filename,FileMode.Open, FileAccess.Read);
+            while ((count = await fs.ReadAsync(fileBuffer, token)) != 0)
             {
-                int offset = i * RecordSize;    
-                bool isEndRecord = fileBuffer.AsSpan(offset, RecordSize).SequenceEqual(EndRecord);
-                if (isEndRecord)
+                for (int i = 0; i < count / RecordSize; i++)
                 {
-                    sha1.TransformFinalBlock(fileBuffer, 0, 0);
-                    goto exit;  // The one acceptable use of goto :)
-                }
-                else
-                {
-                    sha1.TransformBlock(fileBuffer, offset, RecordSize, null, 0);
+                    int offset = i * RecordSize;
+                    bool isEndRecord = fileBuffer.AsSpan(offset, RecordSize).SequenceEqual(EndRecord);
+                    if (isEndRecord)
+                    {
+                        sha1.TransformFinalBlock(fileBuffer, 0, 0);
+                        goto exit;  // The one acceptable use of goto :)
+                    }
+                    else
+                    {
+                        sha1.TransformBlock(fileBuffer, offset, RecordSize, null, 0);
+                    }
                 }
             }
-        }
 
-    exit:
-        return sha1.Hash ?? Array.Empty<byte>();
+        exit:
+            return sha1.Hash ?? [];
+        }
+        catch (CryptographicUnexpectedOperationException)
+        {
+            return [];
+        }
     }
 }
