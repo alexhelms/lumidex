e405543ec5fa9a197f6edf89d88d43631cd96ff0
Alex Helms <alexander.helms@gmail.com>
2024-12-23 00:34:41 -0700

    Coordinate search filter. (#104)

---
+++ Lumidex.Core/Data/Migrations/20241223045525_RaDecRTree.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lumidex.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RaDecRTree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Create the rtree table.
+            migrationBuilder.Sql(
+                sql: """
+                CREATE VIRTUAL TABLE RaDecRTree USING rtree(
+                    Id,
+                    MinRa, MaxRa,
+                    MinDec, MaxDec
+                );
+
+                CREATE TRIGGER IF NOT EXISTS AddRaDecToRTreeAfterInsert
+                	AFTER INSERT
+                	ON ImageFiles
+                	FOR EACH ROW
+                	WHEN 1 = 1
+                		AND NEW.RightAscension IS NOT NULL
+                		AND NEW.Declination IS NOT NULL
+                BEGIN
+                	INSERT INTO RaDecRTree
+                	VALUES (NEW.Id, NEW.RightAscension, NEW.RightAscension, NEW.Declination, NEW.Declination);
+                END;
+
+                CREATE TRIGGER IF NOT EXISTS UpdateRaDecToRTreeAfterUpdate
+                	AFTER UPDATE
+                	ON ImageFiles
+                	FOR EACH ROW
+                	WHEN 1 = 1
+                        AND OLD.Id = NEW.Id
+                		AND NEW.RightAscension IS NOT NULL
+                		AND NEW.Declination IS NOT NULL
+                BEGIN
+                	UPDATE RaDecRTree
+                	SET MinRa = NEW.RightAscension,
+                		MaxRa = NEW.RightAscension,
+                		MinDec = NEW.Declination,
+                		MaxDec = NEW.Declination
+                	WHERE Id = NEW.Id;
+                END;
+
+                CREATE TRIGGER IF NOT EXISTS DeleteRaDecToRTreeAfterDelete
+                	AFTER DELETE
+                	ON ImageFiles
+                	FOR EACH ROW
+                BEGIN
+                	DELETE FROM RaDecRTree
+                	WHERE Id = OLD.Id;
+                END;
+
+                """);
+
+            // Populate RaDecRTree from existing data.
+            migrationBuilder.Sql(
+                sql: """
+                INSERT INTO RaDecRTree
+                SELECT Id, RightAscension as MinRa, RightAscension as MaxRa, Declination as MinDec, Declination as MaxDec
+                FROM (
+                	SELECT Id, RightAscension, Declination
+                	FROM ImageFiles
+                	WHERE 1 = 1
+                		AND RightAscension IS NOT NULL
+                		AND Declination IS NOT NULL
+                );
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RaDecRTree");
+
+            migrationBuilder.Sql(
+                sql: """
+                DROP TRIGGER AddRaDecToRTreeAfterInsert;
+                DROP TRIGGER UpdateRaDecToRTreeAfterUpdate;
+                DROP TRIGGER DeleteRaDecToRTreeAfterDelete;
+                """);
+        }
+    }
+}
---
+++ Lumidex.Core/Data/RaDecSearch.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lumidex.Core.Data;
+
+public static class RaDecSearch
+{
+    public static IQueryable<ImageFile> Search(this LumidexDbContext dbContext, double ra, double dec, double radius)
+    {
+        ra = Math.Clamp(ra, 0, 360);
+        dec = Math.Clamp(dec, -90, 90);
+        radius = Math.Clamp(radius, 0, double.MaxValue);
+
+        FormattableString sql = $"""
+            WITH const as (SELECT {radius} as radius, {ra} as RA, {dec} as DEC)
+            SELECT *
+            FROM ImageFiles, const
+            WHERE Id IN (
+            	SELECT Id
+            	FROM RaDecRTree
+            	WHERE 1 = 1
+            		AND MinRa >= (const.RA - const.radius)
+            		AND MaxRa <= (const.RA + const.radius)
+            		AND MinDec >= (const.DEC - const.radius)
+            		AND MaxDec <= (const.DEC + const.radius)
+            )
+            """;
+
+        return dbContext.ImageFiles
+            .FromSql(sql)
+            .AsNoTracking();
+    }
+}
